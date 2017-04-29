b5197fb01300e4573a00694b65cc592a3379086e
zoomic <marcin.lichota@gmail.com>
2017-04-29 19:37:06 +0200

    base react expamle

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,37 +1,26 @@
-import React from "react"
-import { connect } from "react-redux"
+import React from "react";
 
-import { fetchUser } from "../actions/userActions"
-import { fetchTweets } from "../actions/tweetsActions"
+import Footer from "./Footer";
+import Header from "./Header";
 
-@connect((store) => {
-  return {
-    user: store.user.user,
-    userFetched: store.user.fetched,
-    tweets: store.tweets.tweets,
-  };
-})
 export default class Layout extends React.Component {
-  componentWillMount() {
-    this.props.dispatch(fetchUser())
+  constructor() {
+    super();
+    this.state = {
+      title: "Welcome",
+    };
   }
 
-  fetchTweets() {
-    this.props.dispatch(fetchTweets())
+  changeTitle(title) {
+    this.setState({title});
   }
 
   render() {
-    const { user, tweets } = this.props;
-
-    if (!tweets.length) {
-      return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
-    }
-
-    const mappedTweets = tweets.map(tweet => <li>{tweet.text}</li>)
-
-    return <div>
-      <h1>{user.name}</h1>
-      <ul>{mappedTweets}</ul>
-    </div>
+    return (
+      <div>
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+        <Footer />
+      </div>
+    );
   }
 }
